2096ae2044f3f1e5c09fcf396ac56c231c1c2f32
unknown <binobajichiku@gmail.com>
2023-08-06 20:31:22 +0530

    added logo

--- final_semester_project/src/components/Pdf/UserCardDetails.jsx
+++ final_semester_project/src/components/Pdf/UserCardDetails.jsx
@@ -1,24 +1,42 @@
 import React, { useState } from 'react';
 import { saveAs } from 'file-saver';
-import PdfTemplate from './PdfTemplate';
+// import PdfTemplate from './PdfTemplate';
+import jsPDF from "jspdf"
 
 const UserCardDetails = () => {
   const [user,setUser]=useState({email:"Uttam@gmail.com",name:"Uttam"})
-  const handleDownloadPDF = () => {
-    // Create a PDF blob from the PDF template component
-    const pdfBlob = PdfTemplate().toBlob();
+  // const handleDownloadPDF = () => {
+  //   // Create a PDF blob from the PDF template component
+  //   const pdfBlob = PdfTemplate().blob();
 
-    // // Save the blob as a PDF file using file-saver
-    // saveAs(pdfBlob, 'user_details.pdf');
+  //   // // Save the blob as a PDF file using file-saver
+  //   // saveAs(pdfBlob, 'user_details.pdf');
+  // };
+
+  const downloadPDF = () => {
+    const pdf = new jsPDF();
+    const userDetails = {
+      name: "John Doe",
+      email: "john.doe@example.com",
+      age: 30,
+      address: "123 Main St, City",
+    };
+    pdf.text(20, 20, "User Details Card");
+    pdf.text(20, 30, `Name: ${userDetails.name}`);
+    pdf.text(20, 40, `Email: ${userDetails.email}`);
+    pdf.text(20, 50, `Age: ${userDetails.age}`);
+    pdf.text(20, 60, `Address: ${userDetails.address}`);
+    pdf.save("Id_card.pdf");
   };
-
+  
   return (
     <div>
       <h2>User Details</h2>
       <p>Name: {user.name}</p>
       <p>Email: {user.email}</p>
       {/* Add more user details as needed */}
-      <button onClick={handleDownloadPDF}>Download PDF</button>
+      <button onClick={downloadPDF}>Download as PDF</button>
+     
     </div>
   );
 };
