0a39573993d42e884a4a7ae2a2afd9e7bf7c8ca0
unknown <binobajichiku@gmail.com>
2023-08-13 22:10:52 +0530

    added video selection

--- final_semester_project/src/components/Videopage/VideoThumnails.jsx
+++ final_semester_project/src/components/Videopage/VideoThumnails.jsx
@@ -22,10 +22,24 @@ function getData(url){
 
   return (
     <div>
+      <div>
+        <div>
+          <select name="" id="">
+            <option value="Bca 1st Semester">1st Semseter</option>
+            <option value="Bca 2nd Semester">2nd Semester</option>
+            <option value="Bca 3rd Semester">3rd Semester</option>
+            <option value="Bca 4th semester">4th Semester</option>
+            <option value="Bca 5th Semester">5th Semester</option>
+            <option value="Bca 6th Semester">6th Semester</option>
+          </select>
+        </div>
+      </div>
+    <div>
 
         {data.length===0?<Loading/>:data.map((el,i)=>{
       <Link to="/play"> <img src={el.snippet.thumbnails} alt="el.snippet.channelId" /></Link> 
         })}
+    </div>
     </div>
   )
 }
