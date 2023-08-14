5966b8eb1fff41e4d9c01f198c31c65f81262dcf
unknown <binobajichiku@gmail.com>
2023-08-14 21:26:36 +0530

    embed

--- final_semester_project/src/components/Videopage/VideoPlaySingle.jsx
+++ final_semester_project/src/components/Videopage/VideoPlaySingle.jsx
@@ -3,10 +3,16 @@ import { useParams } from 'react-router-dom'
 
 export default function VideoPlaySingle() {
     const {id}=useParams();
-    console.log(id);
+    // console.log(id);
+    useEffect(()=>{
+        
+    })
+
+
+
   return (
     <div>
-      <iframe width="640" height="360" src="https://www.youtube.com/embed/JQlBt1bXUUc" title="Deewane Hum Nahi Hote Deewani Raat Aati Hai | Stebin Ben ( Selfie) Akshay Kumar |Jacqueline,Emraan H" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+      <iframe width="640" height="360" src="https://www.youtube.com/embed/JQlBt1bXUUc" title="" allowfullscreen></iframe>
     </div>
   )
 }
